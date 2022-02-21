b5360c6bd0bb64e24f5c2ac52d8098e8cb1f4311
Severino de Souza Souza <severino_souza@outlook.com>
2022-02-20 21:45:20 -0300

    primeiro commit

---
+++ BityBank/01-BityBank/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_BityBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
