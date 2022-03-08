7aa8dd6bc58a3951a8c3b45ef6027fdeecb21ee4
severino de souza souza <severino_souza@outlook.com>
2022-03-08 01:39:28 -0400

    feature: add function depositar, sacar e transferir

--- BityBank/01-BityBank/ContaCorrente.cs
+++ BityBank/01-BityBank/ContaCorrente.cs
@@ -4,4 +4,30 @@
     public int agencia;
     public int numero;
     public double saldo;
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool Sacar(double valor)
+    {
+        if(this.saldo < 0)
+        {
+            return false;
+        }
+        this.saldo -= valor;
+        return true;
+    }
+
+    public bool Transferencia(double valor, ContaCorrente Destino)
+    {
+        if(this.saldo < valor)
+        {
+            return false;
+        }
+        this.saldo -= valor;
+        Destino.Depositar(valor);
+        return true;
+    }
 }
